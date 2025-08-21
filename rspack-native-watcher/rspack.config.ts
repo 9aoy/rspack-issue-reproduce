159bb8953cf6023243912cdd485f9098cce982b1
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-08-21 19:40:49 +0800

    demo update

--- rspack-native-watcher/rspack.config.ts
+++ rspack-native-watcher/rspack.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "@rspack/cli";
 import { rspack } from "@rspack/core";
-import { join } from 'node:path';
+import { join } from "node:path";
 
 // Target browsers, see: https://github.com/browserslist/browserslist
-const targets = ["last 2 versions", "> 0.2%",  "not dead",  "Firefox ESR"];
+const targets = ["last 2 versions", "> 0.2%", "not dead", "Firefox ESR"];
 
 class TestFileWatchPlugin {
   private contextToWatch: string | null = null;
@@ -14,7 +14,7 @@ class TestFileWatchPlugin {
 
   apply(compiler: typeof rspack.Compiler.prototype) {
     compiler.hooks.afterCompile.tap(
-      'Rstest:TestFileWatchPlugin',
+      "Rstest:TestFileWatchPlugin",
       (compilation) => {
         if (this.contextToWatch === null) {
           return;
@@ -24,69 +24,76 @@ class TestFileWatchPlugin {
         if (!contextDep.has(this.contextToWatch)) {
           contextDep.add(this.contextToWatch);
         }
-      },
+      }
     );
   }
 }
 
 export default defineConfig({
-	entry: {
-		main: "./src/index.ts"
-	},
-	resolve: {
-		extensions: ["...", ".ts"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: "asset"
-			},
-			{
-				test: /\.js$/,
-				use: [
-					{
-						loader: "builtin:swc-loader",
-						options: {
-							jsc: {
-								parser: {
-									syntax: "ecmascript"
-								}
-							},
-							env: { targets }
-						}
-					}
-				]
-			},
-			{
-				test: /\.ts$/,
-				use: [
-					{
-						loader: "builtin:swc-loader",
-						options: {
-							jsc: {
-								parser: {
-									syntax: "typescript"
-								}
-							},
-							env: { targets }
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [new rspack.HtmlRspackPlugin({ template: "./index.html" }), new TestFileWatchPlugin(join(__dirname, 'tests'))],
-	optimization: {
-		minimizer: [
-			new rspack.SwcJsMinimizerRspackPlugin(),
-			new rspack.LightningCssMinimizerRspackPlugin({
-				minimizerOptions: { targets }
-			})
-		]
-	},
-	experiments: {
-		css: true,
-		nativeWatcher: true,
-	}
+  entry: {
+    main: "./src/index.ts",
+  },
+  resolve: {
+    extensions: ["...", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "ecmascript",
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({ template: "./index.html" }),
+    new TestFileWatchPlugin(join(__dirname, "tests")),
+    // new TestFileWatchPlugin(join(__dirname, "src")),
+  ],
+  optimization: {
+    minimizer: [
+      new rspack.SwcJsMinimizerRspackPlugin(),
+      new rspack.LightningCssMinimizerRspackPlugin({
+        minimizerOptions: { targets },
+      }),
+    ],
+  },
+  experiments: {
+    css: true,
+    nativeWatcher: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 0,
+  }
 });
